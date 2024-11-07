5e2b4b5a2d6f40792b9f6ff66822994bb476e14a
Zam <muxa63@gmail.com>
2024-11-07 13:09:41 +0400

    2.1.3a

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,9 @@
+package app.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component("dog")
+public class Dog extends Animal {
+
+}
